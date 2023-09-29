54aca71c9cc1b843db8e570c7214c848d59e7cc8
Uzann <Uzann@DESKTOP-EQ3MGO4>
2023-09-29 10:10:30 -0300

    imagens do usuario xbox salvando

--- script/artigo.js
+++ script/artigo.js
@@ -12,18 +12,39 @@ document.addEventListener('DOMContentLoaded', async () => {
       const response = await fetch(`/get-article-by-id/${id}`);
       if (response.ok) {
         const articleData = await response.json();
-        const { titulo, conteudo, descricao, imagem_url } = articleData;
-
+        console.log(articleData);
+        const { titulo, conteudo,login_usu, descricao, imagem_url, gamertag, gamerscore, imagem_url_xbox } = articleData;
+       
         const tituloElement = document.querySelector('.noticia-titulo');
         const conteudoElement = document.querySelector('.noticia-conteudo');
         const descricaoPerfilElement = document.querySelector('.descricao_perfil');
         const imagemPerfil = document.querySelector('.imagem-perfil');
+        const gamertagElement = document.querySelector('.gamertag');
+        const gamerscoreElement = document.querySelector('.gamerscore');
      //   const imagemPerfilElement = document.querySelector('.imagem_perfil');
 
+          
+        if(gamertag){
+          gamertagElement.textContent = `Gamertag: ${gamertag}`;
+         }
+
+         if(gamerscore){
+          gamerscoreElement.textContent = gamerscore;
+         }
+         
         tituloElement.textContent = titulo;
         conteudoElement.innerHTML = conteudo;
-        descricaoPerfilElement.textContent = descricao;
-        imagemPerfil.src = imagem_url;
+
+
+// Verifica se a imagem do usuário comum é nula ou vazia
+if (!imagem_url) {
+  imagemPerfil.src = imagem_url_xbox; // Se for nula ou vazia, usa a imagem do Xbox
+} else {
+  imagemPerfil.src = imagem_url; // Caso contrário, usa a imagem do usuário comum
+}
+
+      //  descricaoPerfilElement.textContent = descricao;
+        //imagemPerfil.src = imagem_url;
     //    imagemPerfilElement.src = imagem_url;
     //    imagemPerfilElement.alt = `Imagem`; // Defina uma descrição adequada para a imagem
       } else {
