00e397c22354739dfb815e038a3ec43efc44e33f
Uzann <Uzann@DESKTOP-EQ3MGO4>
2023-12-08 12:44:21 -0300

    Nova fonte de letras adicionada

--- script/articleRedirect.js
+++ script/articleRedirect.js
@@ -5,7 +5,5 @@ async function redirectToArticlePage() {
   });
   
 }
-
-
 redirectToArticlePage();
 /*  export{redirectToArticlePage};*/
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const port = 3000;
 const app = express();
 const dbConfig = require('./script/dbConfig');
 const config = require('./script/xbApiConfig.js');
+const cors = require('cors');
+
 
 // Use as chaves do arquivo de configuração
 const client = XboxApiClient({
@@ -30,7 +32,7 @@ app.use(
   })
 );
 
-
+app.use(cors());
 
 app.use(express.json({ limit: '200mb' }));
 
@@ -43,6 +45,8 @@ app.use(fileUpload());
 app.use('/css', express.static(path.join(__dirname, 'css')));
 
 app.use(express.static(path.join(__dirname, 'rsc')));
+
+app.use(express.static(path.join(__dirname, 'fonts')));
 
 app.use(express.static(path.join(__dirname, 'images-preview')));
 
